8df7ab99f67252541c8ead41e0037f2b104ca359
Sudipta Kundu <sdptknd@gmail.com>
2020-08-30 13:19:42 +0530

    something

--- src/main/java/com/maths/Calculator.java
+++ src/main/java/com/maths/Calculator.java
@@ -1,10 +1,24 @@
 package com.maths;
 
+/**
+ * a calculator
+ */
 public class Calculator {
+  /**
+   * @param num1 the number to add
+   * @param num2 the number to add
+   * @return added value
+   * This is a cool method
+   */
   public static double add(double num1, double num2) {
     return num1 + num2;
   }
 
+  /**
+   * @param num1 the number to add
+   * @param num2 the number to add
+   * @return sub value
+   */
   public static double sub(double num1, double num2) {
     return num1 - num2;
   }
